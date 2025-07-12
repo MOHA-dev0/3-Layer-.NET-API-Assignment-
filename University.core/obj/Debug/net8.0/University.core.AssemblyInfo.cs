82ed8ea814cbd900039876c9afe9053eca89889f
MOHA-dev0 <m.dgamer1975@gmail.com>
2025-07-12 18:19:18 +0300

    v2

--- University.core/obj/Debug/net8.0/University.core.AssemblyInfo.cs
+++ University.core/obj/Debug/net8.0/University.core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("University.core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+951011ebc9cafb6563fcf2a35f0b44b5b03559df")]
 [assembly: System.Reflection.AssemblyProductAttribute("University.core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("University.core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
